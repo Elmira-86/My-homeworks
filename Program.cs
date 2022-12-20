86c24d1e941ae28d17a47d4a26bc1d51e377d9d8
Elmira-86 <120679545+Elmira-86@users.noreply.github.com>
2022-12-20 21:22:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿//Домашнее задание 1
+
+// Console.WriteLine("Input first number");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// int max = a;
+// if (b > max)
+// {  
+//     Console.WriteLine($"{b} is max of {a}");
+// }
+// else
+// {
+//     Console.WriteLine($"{a} is max of {b}");
+// }
+
+//Домашнее задание 2 (Имеется ошибка, не решен)
+
+// Console.WriteLine("Напиши первый номер");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Напиши второй номер");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Напиши третий номер");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// int max = num1;
+
+// if (num2 > max)
+// {  
+//     max = num2;
+// }
+// if (num3 > max)
+// {
+//     max = num3;
+// }
+
+//     Console.WriteLine("Mаксимальное число " + max);
+
+
+// Домашнее задание 3
+
+// Console.WriteLine("Введите число");
+// int i = Convert.ToInt32(Console.ReadLine());
+
+// if (i % 2 == 0)
+// {
+//     Console.WriteLine("Введенное число является четным");
+// }
+// else
+// {
+//     Console.WriteLine("Введенное число нечетное");
+// }
+
+// Домашнее задание 4
+
+// Console.WriteLine("Введите число");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// int res = 1; 
+// if (res == 1)
+// {
+//     res += 1;
+// }
+// while (res <= N)
+// {
+//     Console.WriteLine("Чётные число " + res);
+//     res += 2;
+// }
+
+  
+
+
+ 
